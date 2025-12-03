a155973a1202582dd1be6c2ef81f8ef5558c3456
notiks31 <rree1369s@gmail.com>
2025-12-03 19:05:33 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// config.js
+
+/**
+ * 프로젝트에서 사용하는 모든 API 키를 모아 관리합니다.
+ */
+const API_KEYS = {
+    // 카카오 지도 JavaScript API 키
+    KAKAO_MAP_JAVASCRIPT_KEY: "d6a9086272b0c6871f25e0567fa6305b",
+    
+    // TMAP API 키 (경로 검색에 사용됨)
+    TMAP_API_KEY: "SU3GuwqkSG1U3f8jzfcnY2MkpYvJxYBy9zhoxfSo",
+    
+    // 버스 도착 정보 등 대중교통 관련 API 키
+    BUS_API_KEY: "1fe0be0bcba2e3e38d12218b55ae1e841ae702d4290b2b21c9926f38d7c34c1b"
+};
