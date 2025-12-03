6deb3e54305bb7552cad11a8bb5239067e62bb7d
notiks31 <rree1369s@gmail.com>
2025-12-03 22:57:32 +0900

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const API_KEYS = {
     KAKAO_MAP_JAVASCRIPT_KEY: "d6a9086272b0c6871f25e0567fa6305b",
     
     // TMAP API 키 (경로 검색에 사용됨)
-    TMAP_API_KEY: "SU3GuwqkSG1U3f8jzfcnY2MkpYvJxYBy9zhoxfSo",
+    TMAP_API_KEY: "gnU5nrLHve4KWYpaAWEJV5Tfuiw37F1w63icafR9",
     
     // 버스 도착 정보 등 대중교통 관련 API 키
     BUS_API_KEY: "1fe0be0bcba2e3e38d12218b55ae1e841ae702d4290b2b21c9926f38d7c34c1b"
